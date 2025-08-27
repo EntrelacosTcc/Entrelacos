7a7763525ff47dc4d06d8c687246d930f11d9a31
JuliaDNeves <julialimanevess@gmail.com>
2025-08-27 20:11:12 -0300

    feat: adicionar carrossel de depoimentos na página Sobre Nós

--- js/sobre.js
+++ js/sobre.js
@@ -1,1 +1,13 @@
 // Funcionamento Carrossel Depoimentos
+
+function direcao(e) {
+  var direcao = document.getElementById("content-slide");
+
+  if (e == 1) {
+    // Esquerda
+    direcao.scrollBy({ left: -360, behavior: 'smooth' });
+  } else if (e == 2) {
+    // Direita
+    direcao.scrollBy({ left: 360, behavior: 'smooth' });
+  }
+}
