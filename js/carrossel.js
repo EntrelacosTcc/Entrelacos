23e582aed5c9afe66f79fc49e35bfb6c34b350fe
JuliaDNeves <julialimanevess@gmail.com>
2025-08-30 15:11:13 -0300

    refactor: unificação de dois arquivos carrossel.js em um só, criando um novo parâmetro na função

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,13 +1,13 @@
 // Funcionamento Carrossel Depoimentos
 
-function direcao(e) {
+function direcao(e, qnt) {
   var direcao = document.getElementById("content-slide");
 
   if (e == 1) {
     // Esquerda
-    direcao.scrollBy({ left: -360, behavior: 'smooth' });
+    direcao.scrollBy({ left: -(qnt), behavior: 'smooth' });
   } else if (e == 2) {
     // Direita
-    direcao.scrollBy({ left: 360, behavior: 'smooth' });
+    direcao.scrollBy({ left: qnt, behavior: 'smooth' });
   }
 }
