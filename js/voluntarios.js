dceb9434cf5e08739f0ec1c5fe07184be1205dc7
JuliaDNeves <julialimanevess@gmail.com>
2025-08-30 15:00:48 -0300

    feat: adição do FAQ na página ONGs

--- js/voluntarios.js
+++ js/voluntarios.js
@@ -11,15 +11,3 @@ function proximaImg() {
     if(cont > 2) cont = 1;
     document.getElementById('radio' + cont).checked = true;
 }
-
-
-// perguntas frequentes
-function mostrarEsconder(id) {
-    const conteudo = document.querySelector(id);
-    //faz aparecer o texto ao clicar no +
-    if (conteudo.style.maxHeight && conteudo.style.maxHeight !== "0px") {
-        conteudo.style.maxHeight = "0px"; 
-    } else {
-        conteudo.style.maxHeight = conteudo.scrollHeight + "px"; 
-    }
-}
