8aa207101f017c8015963743cd488a4c196d3306
JuliaDNeves <julialimanevess@gmail.com>
2025-09-01 21:31:09 -0300

    fix: ajuste no código da página Voluntários

--- src/js/carrossel.js
+++ src/js/carrossel.js
@@ -11,3 +11,15 @@ function direcao(e, qnt) {
     direcao.scrollBy({ left: qnt, behavior: 'smooth' });
   }
 }
+
+function direcao1(e, qnt) {
+  var direcao1 = document.getElementById("content-slide1");
+
+  if (e == 1) {
+    // Esquerda
+    direcao1.scrollBy({ left: -(qnt), behavior: 'smooth' });
+  } else if (e == 2) {
+    // Direita
+    direcao1.scrollBy({ left: qnt, behavior: 'smooth' });
+  }
+}
