7251d8582dcf20dd4c5b2d8e9bd21c389e8f8784
Daniele Silva <danielesilvabarroso@gmail.com>
2025-09-05 18:40:59 -0300

    feat: alteração na pasta config

--- config/database.js
+++ config/database.js
@@ -1,12 +1,10 @@
 const mysql = require('mysql2');
 require('dotenv').config(); // Isso carrega as variáveis do .env
 
-module.exports = {
-  connection: {
-    host: process.env.MYSQL_URL ? new URL(process.env.MYSQL_URL).hostname : 'localhost',
-    user: process.env.MYSQL_URL ? new URL(process.env.MYSQL_URL).username : 'root',
-    password: process.env.MYSQL_URL ? new URL(process.env.MYSQL_URL).password : 'DS153010Bar@',
-    database: process.env.MYSQL_URL ? new URL(process.env.MYSQL_URL).pathname.substr(1) : 'db_entrelacos',
-    port: process.env.MYSQL_URL ? new URL(process.env.MYSQL_URL).port : 3306
-  }
-};
+const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+});
