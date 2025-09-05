f8843c57336ed9d569871dd5c73fe29d63a33c9b
Daniele Silva <danielesilvabarroso@gmail.com>
2025-09-05 19:19:43 -0300

    feat: atualização na config

--- config/database.js
+++ config/database.js
@@ -1,10 +1,4 @@
 const mysql = require('mysql2');
 require('dotenv').config(); // Isso carrega as variáveis do .env
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
+const connection = mysql.createConnection(process.env.MYSQL_URL);
