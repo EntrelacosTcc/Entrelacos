fb4a320e1093c184df744c9d115d0a28103f1ba3
JuliaDNeves <julialimanevess@gmail.com>
2025-09-09 21:06:17 -0300

    style: ajuste no script da página de produto específico

--- src/produtos-loja/produtos.js
+++ src/produtos-loja/produtos.js
@@ -2,10 +2,8 @@ const mainImg = document.getElementById("MainImg");
   const smallImg = document.getElementById("small-img");
 
   smallImg.addEventListener("click", () => {
-    // Guarda o src da principal
-    let temp = mainImg.src;
+    let trocarImagem = mainImg.src;
 
-    // Troca os src
     mainImg.src = smallImg.src;
-    smallImg.src = temp;
+    smallImg.src = trocarImagem;
   });
