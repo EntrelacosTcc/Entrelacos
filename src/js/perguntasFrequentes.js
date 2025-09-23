f6b14624f5b5849ec6e4304f7b65e85d3c9deda6
LarissaSantos2 <larissaoliv1395@gmail.com>
2025-09-23 10:08:08 -0300

    feat: adição de pergunta no quiz

--- src/js/perguntasFrequentes.js
+++ src/js/perguntasFrequentes.js
@@ -1,6 +1,6 @@
 function mostrarEsconder(id) {
     const conteudo = document.querySelector(id);
-    //faz aparecer o texto ao clicar no +
+
     if (conteudo.style.maxHeight && conteudo.style.maxHeight !== "0px") {
         conteudo.style.maxHeight = "0px"; 
     } else {
