cfe1a1adf2a4c634809704f4cec41b97eaa5796b
JuliaDNeves <julialimanevess@gmail.com>
2025-09-23 11:25:43 -0300

    feat: resultado do quiz

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -2,7 +2,7 @@ import perguntas from "../js/perguntas-quiz.js";
 
 
 // ===== Configurações =====
-  const NUM_PERGUNTAS = 5;
+  const NUM_PERGUNTAS = 1;
   let selecionadas = [];
   let indiceAtual = 0;
   let pontos = { animal: 0, ambiental: 0, religiosa: 0, GrupoSubRepresentado: 0, social: 0, educacional: 0, saude: 0 };
@@ -109,19 +109,19 @@ import perguntas from "../js/perguntas-quiz.js";
     const vencedor = Object.keys(pontos).reduce((a, b) => pontos[a] > pontos[b] ? a : b);
 
     const textos = {
-      animal: '<p>Você combina com carreiras que envolvem lógica e números (engenharia, estatística, programação).</p><img src="../assets/img/causa-animal.png" alt="Imagem de um gato">',
+      animal: '<p>Sua causa é animal! Seu amor pelos animais é evidente. Seja protegendo pets abandonados ou lutando contra a exploração animal, você acredita que todos os seres vivos merecem respeito e compaixão. Provavelmente já pensou (ou pratica) vegetarianismo ou veganismo, e adora compartilhar vídeos fofos de animais.</p><img src="../assets/img/causa-animal.png" alt="Imagem de um gato">',
 
-      ambiental: '<p>Você combina com carreiras que envolvem lógica e números (engenharia, estatística, programação).</p><img src="../assets/img/causa-ambiental.png" alt="Imagem de um gato">.',
+      ambiental: '<p>Sua causa é ambiental! Você se importa profundamente com o planeta e entende que cada pequena ação conta. Reciclar, economizar água, reduzir o uso de plástico ou simplesmente defender a natureza faz parte do seu dia a dia. Você é daquelas pessoas que não conseguem ver uma árvore sendo cortada sem sentir no peito.</p><img src="../assets/img/causa-ambiental.png" alt="Imagem de um gato">.',
 
-      religiosa: '<p>Você combina com carreiras que envolvem lógica e números (engenharia, estatística, programação).</p><img src="../assets/img/causa-religiosa.png" alt="Imagem de um gato">',
+      religiosa: '<p>Sua causa é a liberdade de fé. Você acredita que cada pessoa deve ter o direito de viver sua espiritualidade, ou sua escolha de não tê-la, com respeito e acolhimento. Para você, é essencial criar espaços de diálogo, compreensão e convivência harmoniosa, onde todas as crenças sejam vistas como legítimas expressões do ser.</p><img src="../assets/img/causa-religiosa.png" alt="Imagem de um gato">',
 
-      GrupoSubRepresentado: '<p>Você combina com carreiras que envolvem lógica e números (engenharia, estatística, programação).</p><img src="../assets/img/causa-animal.png" alt="Imagem de um gato">',
+      GrupoSubRepresentado: '<p>Sua causa é a defesa de grupos sub-representados. Você acredita que pessoas LGBTQIA+, mulheres e PcD merecem respeito, igualdade e visibilidade reais. Para você, não basta empatia: é preciso agir para que ser quem se é, viver com segurança e ter acesso a oportunidades justas deixe de ser privilégio e se torne, de fato, um direito garantido a todos.</p><img src="../assets/img/causa-animal.png" alt="Imagem de um gato">',
 
-      social: '<p>Você combina com carreiras que envolvem lógica e números (engenharia, estatística, programação).</p><img src="../assets/img/causa-social.png" alt="Imagem de um gato">',
+      social: '<p>Sua causa é o cuidado com a sociedade. Você acredita que cada pessoa merece atenção, apoio e oportunidades para viver bem. Para você, o que transforma o mundo é a união, a empatia entre as pessoas e a disposição de ajudar. É sobre estender a mão, valorizar as diferenças e construir, juntos, um caminho mais humano e solidário. </p><img src="../assets/img/causa-social.png" alt="Imagem de um gato">',
 
-      educacional: '<p>Você combina com carreiras que envolvem lógica e números (engenharia, estatística, programação).</p><img src="../assets/img/causa-educacional.png" alt="Imagem de um gato">',
+      educacional: '<p>Sua causa é a educação. Você acredita que o acesso ao conhecimento transforma realidades e abre caminhos para a igualdade de oportunidades. Para você, não basta ensinar: é preciso garantir qualidade, inclusão e condições que permitam a todos aprender e crescer. A educação, em sua visão, é direito essencial e base para a justiça social.</p><img src="../assets/img/causa-educacional.png" alt="Imagem de um gato">',
 
-      saude: '<p>Você combina com carreiras que envolvem lógica e números (engenharia, estatística, programação).</p><img src="../assets/img/causa-saude.png" alt="Imagem de um gato">'
+      saude: '<p>Sua causa é a saúde. Você acredita que cuidar do corpo e da mente não deve ser luxo, mas direito assegurado. Para você, não basta oferecer tratamento: é preciso garantir acesso digno, prevenção e bem-estar. A saúde, em sua visão, é pilar fundamental para que cada pessoa viva com qualidade e dignidade.</p><img src="../assets/img/causa-saude.png" alt="Imagem de um gato">'
     };
 
     resultDiv.innerHTML = `<section class="causa-container"><h2>Sua causa ideal é:</h2><h3>${vencedor.toUpperCase()}</h3><div class="texto-resultado">${textos[vencedor]}</div></section>`;
